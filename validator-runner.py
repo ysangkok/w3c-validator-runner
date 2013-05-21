9af2a6ef41bea6e1d25fd006e4c3462f35c6f4f3
Janus <ysangkok@gmail.com>
2013-05-21 16:21:57 +0200

    first commit

---
+++ validator-runner.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+import lxml
+import lxml.etree
+import lxml.html
+import random
+import io
+import sys
+import itertools
+import subprocess
+import argparse
+import string
+import os
+import json
+
+VALIDATOR_PATH = "/var/www/cgi-bin/w3c-markup-validator/check"
+URL_PREFIX = "http://tyskland.goxadidi.dk:8800/"
+
+def id(N):
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(N)).encode("us-ascii")
+
+def callvalidator(uri, mode):
+    if "get" in mode:
+        env = {"QUERY_STRING": "uri=" + URL_PREFIX + uri, "REQUEST_METHOD": "GET"}
+        s = subprocess.check_output(VALIDATOR_PATH, env=env)
+    else:
+        with open(uri,"rb") as f:
+            inputdata = f.read()
+        i = 1
+        boundary = id(i) 
+        while boundary in inputdata:
+            i += 1
+            boundary = id(i)
+        outputformat = b'json' if "json" in mode else b'html'
+        data = b"".join([b'------' + boundary + b'\r\n' + x for x in [
+            b'Content-Disposition: form-data; name="fragment"\r\n\r\n' + inputdata + b'\r\n', 
+            b'Content-Disposition: form-data; name="prefill"\r\n\r\n0\r\n', 
+            b'Content-Disposition: form-data; name="output"\r\n\r\n' + outputformat + b'\r\n', 
+            b'Content-Disposition: form-data; name="doctype"\r\n\r\nInline\r\n',
+            b'Content-Disposition: form-data; name="prefill_doctype"\r\n\r\nhtml401\r\n',
+            b'Content-Disposition: form-data; name="group"\r\n\r\n0\r\n']]) + b'------' + boundary + b'--\r\n'
+        env = {"CONTENT_LENGTH": str(len(data)), "REQUEST_METHOD": "POST", "CONTENT_TYPE": "multipart/form-data; boundary=----" + boundary.decode("us-ascii")}
+        s = val(data, env)
+        
+    return s
+
+def val(postdata, newenv):
+    p = subprocess.Popen([VALIDATOR_PATH], env=newenv, stderr=subprocess.PIPE, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    return p.communicate(postdata)[0]
+
+def main(txt):
+    ET = lxml.html.document_fromstring(txt)
+    
+    for i in itertools.chain(ET.xpath(".//*[@id='results']"), ET.xpath(".//*[@id='result']"), ET.xpath(".//*[@id='fatal-errors']")):
+        for j in i:
+            c = io.BytesIO()
+            lxml.etree.ElementTree(j).write(c)
+            yield c.getvalue()
+            if b"The document located at" in c.getvalue(): return
+            if b"The uploaded document" in c.getvalue(): return
+            
+def w3mrender(htmltext):
+    stdout, stderr = subprocess.Popen(["w3m", "-dump", "-T", "text/html"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate(htmltext)
+    return stdout
+
+def checkheaderforerror(data):
+    data = data.partition(b"\n\n")[0]
+    data = data.split(b"\n")
+    headers = dict([tuple([x.strip() for x in i.partition(b":")[0::2]]) for i in data])
+    #print(headers)
+    return headers[b"X-W3C-Validator-Status"] == b"Invalid"
+    
+def fin(rawresult):          
+    print(rawresult.decode("utf-8"))
+    sys.exit(haserror)
+
+def prettyjson(rawresult):
+    stolen(json.loads(rawresult.decode("utf-8")))
+
+def stolen(result):
+    errors = 0
+    warnings = 0
+    for msg in result['messages']:
+        if 'lastLine' in msg:
+            print('%(type)s: line %(lastLine)d: %(message)s' % msg)
+        else:
+            print('%(type)s: %(message)s' % msg)
+        if msg['type'] == 'error':
+            errors += 1
+        else:
+            warnings += 1
+
+if __name__ == "__main__":
+    par = argparse.ArgumentParser()
+    par.add_argument("--raw",       help="Upload to validator. Do not parse returned HTML.", action="store_const", const="raw", dest="mode")
+    par.add_argument("--get",       help="Make validator download.        Parse validator HTML.", const="get", action="store_const", dest="mode")
+    par.add_argument("--rawget",    help="Make validator download. Do not parse validator HTML.", const="rawget", action="store_const", dest="mode")
+    par.add_argument("--prettyjson",help="Upload to validator.        Parse returned JSON.", const="prettyjson", action="store_const", dest="mode")
+    par.add_argument("--rawjson",   help="Upload to validator. Do not parse returned JSON.", const="rawjson", action="store_const", dest="mode")
+    par.add_argument("uri",         help="File or URI to validate")
+    parsed = par.parse_args()
+    if not parsed.mode: parsed.mode = "html"
+
+    rawresult = callvalidator(parsed.uri, parsed.mode)
+    haserror = checkheaderforerror(rawresult)
+    if "raw" not in parsed.mode:
+        rawresult = rawresult.partition(b"\n\n")[2]
+        if "json" not in parsed.mode:
+          rawresult = list(main(rawresult))
+          rawresult = b"\n".join(rawresult)
+          rawresult = w3mrender(rawresult)
+          fin(rawresult)
+        elif "prettyjson" == parsed.mode:
+          prettyjson(rawresult)
+        else:
+          fin(rawresult)
+    else:
+        fin(rawresult)
